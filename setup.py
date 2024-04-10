421f22fd447dffb5fd0f0e71212c6c83c49e9f39
Lin Ouyang <74244255+LinOuyang@users.noreply.github.com>
2024-04-10 19:41:15 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
-setup(name='pybarnes',version='0.1.0',author='Lin Ouyang',
+setup(name='pybarnes',version='0.2.0',author='Lin Ouyang',
     packages=["pybarnes"],
     include_package_data=False,
-    install_requires=["metpy"]
+    install_requires=["metpy", "tqdm", "psutil"]
 )
